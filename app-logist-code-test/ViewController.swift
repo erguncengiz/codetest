39ed00f5b2e5f02b608de38ee16bb4821e6a2b7c
Ergün Yunus Cengiz <ergunyunuscengiz@gmail.com>
2022-12-07 10:14:38 +0300

    Initial Commit

---
+++ app-logist-code-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-logist-code-test
+//
+//  Created by Ergün Yunus Cengiz on 7.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
